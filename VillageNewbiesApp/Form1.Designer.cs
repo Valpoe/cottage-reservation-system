b521d39b9a55002b653cd05a2b558a036e91572b
AndersTuomainen <tuomainen.anders@outlook.com>
2022-04-07 14:28:10 +0300

    Add project files.

---
+++ VillageNewbiesApp/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace VillageNewbiesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRaportit = new System.Windows.Forms.Button();
+            this.btnLaskut = new System.Windows.Forms.Button();
+            this.btnAsiakkaat = new System.Windows.Forms.Button();
+            this.btnVaraukset = new System.Windows.Forms.Button();
+            this.btnPalvelut = new System.Windows.Forms.Button();
+            this.btnAlueet = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbVillageNewbies = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.btnRaportit);
+            this.panel1.Controls.Add(this.btnLaskut);
+            this.panel1.Controls.Add(this.btnAsiakkaat);
+            this.panel1.Controls.Add(this.btnVaraukset);
+            this.panel1.Controls.Add(this.btnPalvelut);
+            this.panel1.Controls.Add(this.btnAlueet);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(186, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnRaportit
+            // 
+            this.btnRaportit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRaportit.FlatAppearance.BorderSize = 0;
+            this.btnRaportit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRaportit.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRaportit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnRaportit.Location = new System.Drawing.Point(0, 369);
+            this.btnRaportit.Name = "btnRaportit";
+            this.btnRaportit.Size = new System.Drawing.Size(186, 45);
+            this.btnRaportit.TabIndex = 2;
+            this.btnRaportit.Text = "Raportit";
+            this.btnRaportit.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnRaportit.UseVisualStyleBackColor = true;
+            // 
+            // btnLaskut
+            // 
+            this.btnLaskut.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLaskut.FlatAppearance.BorderSize = 0;
+            this.btnLaskut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLaskut.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLaskut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnLaskut.Location = new System.Drawing.Point(0, 324);
+            this.btnLaskut.Name = "btnLaskut";
+            this.btnLaskut.Size = new System.Drawing.Size(186, 45);
+            this.btnLaskut.TabIndex = 2;
+            this.btnLaskut.Text = "Laskut";
+            this.btnLaskut.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnLaskut.UseVisualStyleBackColor = true;
+            // 
+            // btnAsiakkaat
+            // 
+            this.btnAsiakkaat.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAsiakkaat.FlatAppearance.BorderSize = 0;
+            this.btnAsiakkaat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAsiakkaat.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAsiakkaat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnAsiakkaat.Location = new System.Drawing.Point(0, 279);
+            this.btnAsiakkaat.Name = "btnAsiakkaat";
+            this.btnAsiakkaat.Size = new System.Drawing.Size(186, 45);
+            this.btnAsiakkaat.TabIndex = 2;
+            this.btnAsiakkaat.Text = "Asiakkaat";
+            this.btnAsiakkaat.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAsiakkaat.UseVisualStyleBackColor = true;
+            // 
+            // btnVaraukset
+            // 
+            this.btnVaraukset.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnVaraukset.FlatAppearance.BorderSize = 0;
+            this.btnVaraukset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVaraukset.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVaraukset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnVaraukset.Location = new System.Drawing.Point(0, 234);
+            this.btnVaraukset.Name = "btnVaraukset";
+            this.btnVaraukset.Size = new System.Drawing.Size(186, 45);
+            this.btnVaraukset.TabIndex = 2;
+            this.btnVaraukset.Text = "Varaukset";
+            this.btnVaraukset.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnVaraukset.UseVisualStyleBackColor = true;
+            // 
+            // btnPalvelut
+            // 
+            this.btnPalvelut.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPalvelut.FlatAppearance.BorderSize = 0;
+            this.btnPalvelut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPalvelut.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPalvelut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnPalvelut.Location = new System.Drawing.Point(0, 189);
+            this.btnPalvelut.Name = "btnPalvelut";
+            this.btnPalvelut.Size = new System.Drawing.Size(186, 45);
+            this.btnPalvelut.TabIndex = 2;
+            this.btnPalvelut.Text = "Palvelut";
+            this.btnPalvelut.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnPalvelut.UseVisualStyleBackColor = true;
+            // 
+            // btnAlueet
+            // 
+            this.btnAlueet.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAlueet.FlatAppearance.BorderSize = 0;
+            this.btnAlueet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlueet.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlueet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnAlueet.Location = new System.Drawing.Point(0, 144);
+            this.btnAlueet.Name = "btnAlueet";
+            this.btnAlueet.Size = new System.Drawing.Size(186, 45);
+            this.btnAlueet.TabIndex = 2;
+            this.btnAlueet.Text = "Toiminta-alueet";
+            this.btnAlueet.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAlueet.UseVisualStyleBackColor = true;
+            this.btnAlueet.Click += new System.EventHandler(this.btnAlueet_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.lbVillageNewbies);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(186, 144);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbVillageNewbies
+            // 
+            this.lbVillageNewbies.AutoSize = true;
+            this.lbVillageNewbies.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbVillageNewbies.ForeColor = System.Drawing.Color.LightGray;
+            this.lbVillageNewbies.Location = new System.Drawing.Point(36, 9);
+            this.lbVillageNewbies.Name = "lbVillageNewbies";
+            this.lbVillageNewbies.Size = new System.Drawing.Size(116, 58);
+            this.lbVillageNewbies.TabIndex = 0;
+            this.lbVillageNewbies.Text = "Village\r\nNewbies";
+            this.lbVillageNewbies.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbVillageNewbies;
+        private System.Windows.Forms.Button btnAlueet;
+        private System.Windows.Forms.Button btnRaportit;
+        private System.Windows.Forms.Button btnLaskut;
+        private System.Windows.Forms.Button btnAsiakkaat;
+        private System.Windows.Forms.Button btnVaraukset;
+        private System.Windows.Forms.Button btnPalvelut;
+    }
+}
+
---
+++ VillageNewbiesApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace VillageNewbiesApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+    (
+        int nLeftRect,
+        int nTopRect,
+        int nRightRect,
+        int nBottomRect,
+        int nWidthEllipse,
+        int nHeightEllipse
+    );
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlueet_Click(object sender, EventArgs e)
+        {
+            pnlNavigation.Height = btnAlueet.Height;
+            pnlNav.Top = btnAlueet.Top;
+            pnlNav.Left = btnAlueet.Left;
+            btnAlueet.BackColor = Color.FromArgb(46, 51, 73);
+        }
+    }
+}
