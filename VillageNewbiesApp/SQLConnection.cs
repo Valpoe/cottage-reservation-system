293c70baf3d98556ec8e482f4319cb46ad6e828f
Mikemu Mikemu <mikko.rautava@outlook.com>
2022-04-18 13:38:57 +0300

    SQL classi lisätty

---
+++ VillageNewbiesApp/SQLConnection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace VillageNewbiesApp
+{
+    class SQLConnection
+    {
+
+        public static MySqlConnection GetConnection()
+        {
+            string cString = "datasource=localhost;port=3307;username=root;password=Ruutti;database=vn";
+            MySqlConnection connectionString = new MySqlConnection(cString);
+            try
+            {
+                connectionString.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Yhdistäminen epäonnistui, " + ex.Message);
+            }
+
+            return connectionString;
+        }
+
+        public void SQLselectAll(string select)
+        {
+
+            using (MySqlConnection connection = GetConnection())
+            {
+                MessageBox.Show("Success, nyt tietokanta on avattu turvallisesti using statementilla!");
+                MySqlCommand Command = new MySqlCommand("SELECT * FROM " + select, connection);
+                MySqlDataReader Reader = Command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    MessageBox.Show(Reader.GetString(Reader.GetOrdinal("etunimi")));
+                }
+            }
+
+            Console.WriteLine("connection closed");
+        }
+    }
+
+
+}
